afdd2af902e64b8536a425b1003ea6ab60f355ed
lauracurley <info@lauracurley.com>
2016-03-16 20:11:01 -0700

    Play song when it is the only one in the queue.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,17 @@
 var SongQueue = Songs.extend({
 
   initialize: function() {
-  }
+    this.on('add', this.playFirst);
+    // this.on('change:length', )
+  },
+  playFirst: function() {
+    /*console.dir(this.length);
+    if (this.length === 1) {
+      this.trigger('playFirst', this);
+    } else {
+      // this.trigger('notPlayFirst', this);
+    }*/
+  },
+  notPlayFirst: function(){}
 
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -22,7 +22,7 @@ var AppModel = Backbone.Model.extend({
     // this.songQueue
 
     params.library.on('enqueue', function(song) {
-      this.get("songQueue").add(song);
+      this.get('songQueue').add(song);
     }, this);
   }
 
